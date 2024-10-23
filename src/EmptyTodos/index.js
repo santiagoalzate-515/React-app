74964089b1dbb2f5b1c77bf49a06f30f47100e33
santiago alzate <alzatesantiago515@gmail.com>
2024-10-23 16:26:47 -0500

    commit arreglo empty

--- src/EmptyTodos/index.js
+++ src/EmptyTodos/index.js
@@ -3,7 +3,7 @@ import "./EmptyTodos.css"
 
 function EmptyTodos() {
     return(
-        <p className="empty">Crea Tu Primer TODO...</p>
+        <p className="empty">Crea Tu Primer NOTA...</p>
     )
 }
 
--- src/TodoItem/Todocontainer.js
+++ src/TodoItem/Todocontainer.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function Todocontainer({children}) {
     return(
         <span>
